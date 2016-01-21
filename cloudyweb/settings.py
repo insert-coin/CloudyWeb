bf69e0425b4fba9ce454708affab6826e8575c3a
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-01-22 02:25:31 +0800

    Fix indent

--- cloudyweb/settings.py
+++ cloudyweb/settings.py
@@ -116,6 +116,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-	'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
 }

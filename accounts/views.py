d15a40b3453520039665c4e45891f1510d81c77f
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-03-05 05:14:01 +0800

    Token will be regenerated after the old one is given

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets
 from rest_framework import permissions as drf_permissions
+from rest_framework import parsers, renderers
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import serializers
 from . import permissions
@@ -23,3 +28,20 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        Token.objects.filter(user=user).delete()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key})
+
+
+obtain_auth_token = ObtainAuthToken.as_view()

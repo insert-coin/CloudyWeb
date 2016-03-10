b945bc9b5a25d1bc7f64cc6b34bcef56d46a123a
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-03-10 15:52:19 +0800

    Display Admin page

--- cloudygames/admin.py
+++ cloudygames/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register([models.Game, models.GameSession,
+    models.GameOwnership, models.PlayerSaveData])

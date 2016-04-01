bc678e26e32703ed49d645f6eba584a325a33c33
gabriellaMT <gabriellamichelle752@gmail.com>
2016-04-02 02:46:18 +0800

    fix migration problem

--- cloudygames/migrations/0023_auto_20160402_0245.py
+++ cloudygames/migrations/0023_auto_20160402_0245.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import imagekit.models.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cloudygames', '0021_auto_20160314_0100'),
+        ('cloudygames', '0022_auto_20160331_1413'),
     ]
 
     operations = [
@@ -22,16 +21,6 @@ class Migration(migrations.Migration):
             model_name='game',
             name='max_limit',
             field=models.IntegerField(default=4),
-        ),
-        migrations.AlterField(
-            model_name='game',
-            name='thumbnail',
-            field=imagekit.models.fields.ProcessedImageField(upload_to='thumbnails', default='thumbnails/default.png'),
-        ),
-        migrations.AlterField(
-            model_name='playersavedata',
-            name='saved_file',
-            field=models.FileField(upload_to='save_data'),
         ),
         migrations.AlterUniqueTogether(
             name='playersavedata',
